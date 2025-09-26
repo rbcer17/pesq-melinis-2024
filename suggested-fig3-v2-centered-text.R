dbb16b7f405058de4a450ea66e6262719bd0c63e
Roberto Cavalcanti <rbcer10@gmail.com>
2025-09-26 12:01:42 -0300

    usando dados reais para o grafico

--- suggested-fig3-v2-centered-text.R
+++ suggested-fig3-v2-centered-text.R
@@ -51,3 +51,69 @@ final_plot <- (pA + pB) / (pC + pD)
 
 # Show the plot
 print(final_plot)
+
+
+#USING THE REAL DATA
+library(tidyverse)
+#https://www.statology.org/pivot_longer-in-r/
+#pivot the data frame into a long format
+coberturapivot = as.data.frame(data_only_cobertura_2003_a_2005_sent_04may2020)
+coberturapivot1 = coberturapivot %>% pivot_longer(cols=c('total03', 'total04','total05'),
+                                                  names_to='ano',
+                                                  values_to='cobtotal')
+coberturapivot2 = coberturapivot %>% pivot_longer(cols=c('mm03', 'mm04','mm05'),
+                                                  names_to='anomm',
+                                                  values_to='cobmm')
+#remove columns 1 to 5 of coberturapivot2
+coberturapivot2 <- coberturapivot2[ -c(1:5) ]
+#add columns dataframe coberturapivot2 to coberturapivot1
+df2 <- cbind(coberturapivot1, coberturapivot2)
+#recode ano to have just numbers
+df2$anocal = substr(df2$ano,6,7)
+#pivot to have cobertura single variable
+df3 = df2 %>% pivot_longer(cols=c('cobtotal', 'cobmm'),
+                           names_to='tipocob',
+                           values_to='percob')
+#remove cols 3 4 5 6 7 from df3
+df4 <- df3 [ -c(3:7) ]
+
+#agora vamos usar o dataframe df4 para replicar a figura 3 do paper com sigmaplot
+df4$tipocob <- factor(df4$tipocob, levels = c("cobtotal", "cobmm"))
+#mydat_tibble <- mydat_tibble %>% 
+#  rename(Years_From_Diagnosis = Yrs_From_Dx)
+
+con1 <- subset(df4, trat == 1)
+con2 <- subset(df4, trat == 2)
+con3 <- subset(df4, trat == 3)
+con4 <- subset(df4, trat == 4)
+require(gridExtra)
+#BOXPLOTS
+plot1=ggplot(con1, aes(x=anocal,y=percob, fill = tipocob))+ geom_boxplot()
+plot2=ggplot(con2, aes(x=anocal,y=percob, fill = tipocob))+ geom_boxplot()
+plot3=ggplot(con3, aes(x=anocal,y=percob, fill = tipocob))+ geom_boxplot()
+plot4=ggplot(con4, aes(x=anocal,y=percob, fill = tipocob))+ geom_boxplot()
+
+plot1
+grid.arrange(plot1, plot2, plot3, plot4, ncol=2)
+#BARPLOT WITH ERROR BARS FIGURE 3 FINAL
+#calculate mean values of y
+library(dplyr)
+library(ggplot2)
+summary_data2 <- df4 %>%
+  group_by(trat,anocal,tipocob) %>%
+  summarize(
+    percent_cover = mean(percob),
+    sd_value = sd(percob)
+  )
+#Rename the variables according to the plot
+summary_data2 <- summary_data2 %>% rename(coverage = tipocob)
+summary_data2 <- summary_data2 %>% rename(year = anocal)
+library(forcats)
+summary_data2$coverage <- fct_recode(summary_data2$coverage,
+                                     "total" = "cobtotal",  # Renaming "cobtotal" to "total"
+                                     "melinis" = "cobmm")  # Renaming "cobmm" to "melinis"
+summary_data2$year <- fct_recode(summary_data2$year,
+                                 "2003" = "03",  # Renaming "03" to "2003"
+                                 "2004" = "04", # Renaming "04" to "2004"
+                                 "2005" = "05")  # Renaming "05" to "2005"
+
